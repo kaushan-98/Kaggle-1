7bc400571beb55729690fadcfb27b88f623ca9ae
Vihanga Kaushan <106341112+kaushan-98@users.noreply.github.com>
2022-11-12 23:45:53 +0530

    Add files via upload

---
+++ Intro to Deep Learning.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # A Single Neuron
+
+# In[ ]:
+
+
+# Setup plotting
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn-whitegrid')
+# Set Matplotlib defaults
+plt.rc('figure', autolayout=True)
+plt.rc('axes', labelweight='bold', labelsize='large',
+       titleweight='bold', titlesize=18, titlepad=10)
+
+# Setup feedback system
+from learntools.core import binder
+binder.bind(globals())
+from learntools.deep_learning_intro.ex1 import *
+
+
+# In[ ]:
+
+
+import pandas as pd
+
+red_wine = pd.read_csv('../input/dl-course-data/red-wine.csv')
+red_wine.head()
+
+
+# In[ ]:
+
+
+red_wine.shape # (rows, columns)
+
+
+# In[ ]:
+
+
+# YOUR CODE HERE
+input_shape = [11]
+
+
+# In[ ]:
+
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# YOUR CODE HERE
+model = keras.Sequential([
+    layers.Dense(units=1, input_shape=[11])
+])
+
+
+# In[ ]:
+
+
+w, b = model.weights
+
+print("Weights\n{}\n\nBias\n{}".format(w, b))
+
+
+# # Deep Neural Networks
+
+# In[ ]:
+
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+model = keras.Sequential([
+    # the hidden ReLU layers
+    layers.Dense(units=4, activation='relu', input_shape=[2]),
+    layers.Dense(units=3, activation='relu'),
+    # the linear output layer 
+    layers.Dense(units=1),
+])
+
+
+# In[ ]:
+
+
+# Setup plotting
+import matplotlib.pyplot as plt
+from learntools.deep_learning_intro.dltools import animate_sgd
+plt.style.use('seaborn-whitegrid')
+# Set Matplotlib defaults
+plt.rc('figure', autolayout=True)
+plt.rc('axes', labelweight='bold', labelsize='large',
+       titleweight='bold', titlesize=18, titlepad=10)
+plt.rc('animation', html='html5')
+
+# Setup feedback system
+from learntools.core import binder
+binder.bind(globals())
+from learntools.deep_learning_intro.ex3 import *
+
+
+# In[ ]:
+
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import make_column_transformer, make_column_selector
+from sklearn.model_selection import train_test_split
+
+fuel = pd.read_csv('../input/dl-course-data/fuel.csv')
+
+X = fuel.copy()
+# Remove target
+y = X.pop('FE')
+
+preprocessor = make_column_transformer(
+    (StandardScaler(),
+     make_column_selector(dtype_include=np.number)),
+    (OneHotEncoder(sparse=False),
+     make_column_selector(dtype_include=object)),
+)
+
+X = preprocessor.fit_transform(X)
+y = np.log(y) # log transform target instead of standardizing
+
+input_shape = [X.shape[1]]
+print("Input shape: {}".format(input_shape))
+
+
+# In[ ]:
+
+
+
+
